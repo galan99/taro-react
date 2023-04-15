af67d600b5d2cdedf21d8a59b02cb77b8cf97569
anlen.wang <619531862@qq.com>
2023-04-15 17:30:28 +0800

    第二个案例

--- src/components/FormCom/index.tsx
+++ src/components/FormCom/index.tsx
@@ -4,205 +4,147 @@ import React, { useState, useEffect } from 'react';
 import {View, Text } from "@tarojs/components"
 
 
-interface FormItem {
-  label: string;
-  key: string,
+// FormConfig.tsx
+import React, { useState, useEffect } from 'react';
+import { Input, Select } from 'nutui-react';
+
+interface ValidationRule {
+  regex?: RegExp;
+  validator?: (value: any) => boolean;
+  message?: string;
+}
+
+interface FormItemConfig {
   type: 'input' | 'select';
-  inputType?: string;
-  options?: Array<{ text: string; value: string | number }>;
+  label: string;
+  name: string;
+  rules?: ValidationRule[];
   hidden?: boolean;
-  children?: FormItem[];
-  validation?: RegExp | ((value: any) => boolean);
+  options?: { label: string; value: any }[];
+  api?: () => Promise<{ label: string; value: any }[]>;
+  children?: FormConfigProps[];
+  dependencies?: string[];
 }
 
-interface FormProps {
-  formData: FormItem[];
-  onSubmit: (values: { [key: string]: any }) => void;
-  setValues?: any,
-  initialValues?: { [key: string]: any };
-  loadSelectOptions?: (selectName: string) => Promise<Array<{ text: string; value: string | number }>>;
+interface FormConfigProps {
+  items: FormItemConfig[];
+  initialValues?: Record<string, any>;
+  onSubmit?: (values: Record<string, any>) => void;
 }
 
-const FInput = (props) => {
-  const {defaultValue, onChange, placeholder} = props;
-  console.log(defaultValue)
-  const [value, UpdateValue] = useState('')
-
-  const ChangeEvent = (val) => {
-    console.log(defaultValue, val)
-    onChange(val)
-    UpdateValue(val)
-  }
-
-  return (
-    <Input 
-      name='text' 
-      defaultValue={defaultValue}  
-      placeholder={placeholder} 
-      onChange={ChangeEvent}
-    />
-  )
-}
-
-const Select = (props) => {
-  const { options = [], onChange, defaultValue, placeholder = "请选择" } = props;
-
-  const checkText = options.find(item => item.value == defaultValue)?.text || '';
-  const [selectedValue, setSelectedValue] = useState(checkText);
-  const [isVisible, setIsVisible] = useState(false)
-
-
-
-  const confirmPicker = (val = [], data = []) => {
-    setSelectedValue(data[0].text);
-    onChange(val[0], data[0]);
-  };
-
-  return (
-    <View>
-      <Cell title={placeholder} desc={selectedValue} onClick={() => setIsVisible(!isVisible)}/>
-      <Picker
-        isVisible={isVisible}
-        listData={options}
-        defaultValueData={selectedValue}
-        onClose={() => setIsVisible(false)}
-        onConfirm={confirmPicker}
-      />
-    </View>
-  );
-};
-
-
-const Form: React.FC<FormProps> = ({ formData, onSubmit, initialValues, loadSelectOptions }) => {
-  const [formValues, setFormValues] = useState<{ [key: string]: any }>({});
-  console.log(initialValues, formValues, formData)
-  const [errors, setErrors] = useState<{ [key: string]: string }>({});
-  const [selectValues, setSelectValues] = useState<{ [key: string]: Array<{ text: string; value: string | number }> }>({});
+const FormConfig: React.FC<FormConfigProps> = (props) => {
+  const { items, initialValues, onSubmit } = props;
+  const [values, setValues] = useState<Record<string, any>>(initialValues || {});
+  const [errors, setErrors] = useState<Record<string, string>>({});
 
   useEffect(() => {
-    formData.forEach((item) => {
-      if (item.type === 'select' && item.options) {
-        setSelectValues((prevSelectValues: any) => ({
-          ...prevSelectValues,
-          [item.key]: item.options,
-        }));
+    items.forEach((item) => {
+      if (item.type === 'select' && item.api) {
+        item.api().then((options) => {
+          item.options = options;
+        });
       }
     });
-  }, [formData]);
+  }, [items]);
 
-  useEffect(() => {
-    if (loadSelectOptions) {
-      formData.forEach((item) => {
-        if (item.type === 'select' && !item.options) {
-          loadSelectOptions(item.key).then((options) => {
-            setSelectValues((prevSelectValues) => ({
-              ...prevSelectValues,
-              [item.key]: options,
-            }));
-          });
+  const renderFormItem = (item: FormItemConfig) => {
+    const { type, label, name, hidden } = item;
+
+    if (hidden) return null;
+
+    if (type === 'input') {
+      return (
+        <div key={name}>
+          <label>{label}</label>
+          <Input
+            value={values[name] || ''}
+            onChange={(e) => handleValueChange(name, e.target.value)}
+          />
+          {errors[name] && <p>{errors[name]}</p>}
+        </div>
+      );
+    } else if (type === 'select') {
+      return (
+        <div key={name}>
+          <label>{label}</label>
+          <Select
+            options={item.options || []}
+            value={values[name]}
+            onChange={(value) => handleValueChange(name, value)}
+          />
+          {errors[name] && <p>{errors[name]}</p>}
+        </div>
+      );
+    }
+  };
+
+  const handleValueChange = (name: string, value: any) => {
+    setValues((prevValues) => ({ ...prevValues, [name]: value }));
+
+    const item = items.find((i) => i.name === name);
+    if (item) {
+      handleDependencies(item, value);
+      validateField(item, value);
+    }
+  };
+
+  const handleDependencies = (item: FormItemConfig, value: any) => {
+    if (item.dependencies) {
+      item.dependencies.forEach((depName) => {
+        const depItem = items.find((i) => i.name === depName);
+        if (depItem) {
+          depItem.hidden = !value;
         }
       });
     }
-  }, [formData, loadSelectOptions]);
-
-  const handleInputChange = (value: string, name: string) => {
-    setFormValues({
-      ...formValues,
-      [name]: value,
-    });
-    console.log(formValues)
   };
 
-  const handleSelectChange = (value: string | number, name: string) => {
-    setFormValues({
-      ...formValues,
-      [name]: value,
-    });
-    console.log(formValues)
-  };
+  const validateField = (item: FormItemConfig, value: any) => {
+    if (!item.rules) return true;
 
-  const handleBlur = (event: React.FocusEvent<HTMLInputElement>, name: string, item: FormItem) => {
-    if (item.validation) {
-      if (item.validation instanceof RegExp) {
-        if (!item.validation.test(event.target.value)) {
-          setErrors({
-            ...errors,
-
-            [name]: '格式不正确',
-          });
-        } else {
-          setErrors((prevErrors) => {
-            const newErrors = { ...prevErrors };
-            delete newErrors[name];
-            return newErrors;
-          });
-        }
-      } else if (!item.validation(event.target.value)) {
-        setErrors({
-          ...errors,
-          [name]: '格式不正确',
-        });
-      } else {
-        setErrors((prevErrors) => {
-          const newErrors = { ...prevErrors };
-          delete newErrors[name];
-          return newErrors;
-        });
+    let error = '';
+    for (const rule of item.rules) {
+      if (rule.regex && !rule.regex.test(value)) {
+        error = rule.message || '';
+        break;
+      } else if (rule.validator && !rule.validator(value)) {
+        error = rule.message || '';
+       
+        break;
       }
     }
+
+    setErrors((prevErrors) => ({ ...prevErrors, [item.name]: error }));
+
+    return !error;
   };
 
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    const newErrors: { [key: string]: string } = {};
-    formData.forEach((item) => {
-      if (item.type === 'input' && item.validation) {
-        if (item.validation instanceof RegExp) {
-          if (!item.validation.test(formValues[item.key])) {
-            newErrors[item.key] = '格式不正确';
-          }
-        } else if (!item.validation(formValues[item.key])) {
-          newErrors[item.key] = '格式不正确';
-        }
-      }
+  const handleSubmit = () => {
+    const isValid = items.every((item) => {
+      if (item.hidden) return true;
+      return validateField(item, values[item.name]);
     });
-    setErrors(newErrors);
-    if (Object.keys(newErrors).length === 0) {
-      onSubmit(formValues);
+
+    if (isValid && onSubmit) {
+      const submittedValues = items.reduce((acc, item) => {
+        if (!item.hidden) {
+          acc[item.name] = values[item.name];
+        }
+        return acc;
+      }, {} as Record<string, any>);
+      onSubmit(submittedValues);
     }
   };
 
   return (
-    <form>
-      {formData.map((item, index) => (
-        <React.Fragment key={index}>
-          {item.hidden ? null : (
-            <div>
-              <label htmlFor={item.label}>{item.label}</label>
-              {item.type === 'input' ? (
-                <FInput
-                  defaultValue={formValues[item.key] || ''}
-                  onChange={(event) => handleInputChange(event, item.key)}
-                />
-              ) : (
-                <Select
-                  options={item.options}
-                  defaultValue={formValues[item.key] || ''}
-                  onChange={(value) => handleSelectChange(value, item.key)}
-                />
-              )}
-              {errors[item.key] && <div style={{ color: 'red' }}>{errors[item.key]}</div>}
-            </div>
-          )}
-          {item.children ? (
-            <Form formData={item.children} />
-          ) : null}
-        </React.Fragment>
+    <div>
+      {items.map((item) => (
+        <React.Fragment key={item.name}>{renderFormItem(item)}</React.Fragment>
       ))}
-    </form>
+      <button onClick={handleSubmit}>提交</button>
+    </div>
   );
 };
 
-export default Form;
-
+export default FormConfig;
 
